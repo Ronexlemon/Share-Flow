8104ba27a75760a59ec26e536fe4711bdb400e47
RonexLemon <ronexondimu@gmail.com>
2023-06-25 01:29:48 +0300

    implement contract

---
+++ Contract/contracts/test/Shareflow.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+// forge create  --rpc-url  https://rpc.testnet.fantom.network    --priv
+// ate-key <> src/ShareFlow.sol:ShareFlow
+// [⠘] Compiling...
+// No files changed, compilation skipped
+// Deployer: 0x65E28C9C4Ef1a756d8df1c507b7A84eFcF606fd4
+// Deployed to: 0x8baCA7C2B1b0d82238Ca0097e1980498d10ced36
+// Transaction hash: 0x4886c3ba802295de49751e68a6d8742f96116672de487f7c1f9c2127cb4ef136
+// import "forge-std/Test.sol";
+ import "../src/Counter.sol";
+
+contract CounterTest  {
+    Counter public counter;
+}
